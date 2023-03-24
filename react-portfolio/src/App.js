d62735918d606ee749bec64255066033af293b29
Deorren Erive <erive92d@gmail.com>
2023-03-23 17:22:36 -0700

    test

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,14 +1,11 @@
-import './App.css';
-import Header from './components/Header';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import "./App.css";
+import Header from "./components/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-
   return (
     <div className="App">
-      <Header/>
-     
+      <Header />
     </div>
   );
 }
--- react-portfolio/src/components/Header.js
+++ react-portfolio/src/components/Header.js
@@ -1,63 +1,74 @@
-import { useEffect, useState } from "react"
-import About from "./About"
-import Projects from "./Projects"
-import Resume from "./Resume"
-import Contact from "./Contact"
-import Nav from "react-bootstrap/Nav"
+import { useEffect, useState } from "react";
+import About from "./About";
+import Projects from "./Projects";
+import Resume from "./Resume";
+import Contact from "./Contact";
+import Nav from "react-bootstrap/Nav";
 
 function Header() {
+  const [location, setLocation] = useState("about");
 
-    const [location, setLocation] = useState('about')
-
-
-
-    const renderPage = () => {
-        if (location === 'about') {
-            return <About />
-        }
-        if (location === 'projects') {
-            return <Projects />
-        }
-        if (location === 'contacts') {
-            return <Contact />
-        } else {
-            return <Resume />
-        }
+  const renderPage = () => {
+    if (location === "about") {
+      return <About />;
     }
+    if (location === "projects") {
+      return <Projects />;
+    }
+    if (location === "contacts") {
+      return <Contact />;
+    } else {
+      return <Resume />;
+    }
+  };
 
-    const handlePage = (value) => setLocation(value)
+  const handlePage = (value) => setLocation(value);
 
-    return (
-        <div>
-            <Navigation handlePage={handlePage} />
-            {renderPage()}
-        </div>
-    )
+  return (
+    <div>
+      <Navigation handlePage={handlePage} />
+      {renderPage()}
+    </div>
+  );
 }
 
-
-
 function Navigation(props) {
-
-    return (
-            
-         <Nav fill variant="tabs" defaultActiveKey="/home">
+  return (
+    <Nav fill variant="pills" defaultActiveKey="/home">
       <Nav.Item>
-        <Nav.Link href="#about" onClick={()=>props.handlePage('about')}>About</Nav.Link>
+        <Nav.Link href="#about" onClick={() => props.handlePage("about")}>
+          About
+        </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link href="#projects" onClick={()=>props.handlePage('projects')} eventKey="link-1">Projects</Nav.Link>
+        <Nav.Link
+          href="#projects"
+          onClick={() => props.handlePage("projects")}
+          eventKey="link-1"
+        >
+          Projects
+        </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link href="#contacts" onClick={()=>props.handlePage('contacts')} eventKey="link-2">Contacts</Nav.Link>
+        <Nav.Link
+          href="#contacts"
+          onClick={() => props.handlePage("contacts")}
+          eventKey="link-2"
+        >
+          Contacts
+        </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link href="#resume" onClick={()=>props.handlePage('resume')} eventKey="link-3">
+        <Nav.Link
+          href="#resume"
+          onClick={() => props.handlePage("resume")}
+          eventKey="link-3"
+        >
           Resume
         </Nav.Link>
       </Nav.Item>
     </Nav>
-    )
+  );
 }
 
 export default Header;
